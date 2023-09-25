8ea0d17d9de8dd4da46d80291b567510f22c3b70
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-09-25 13:17:14 -0500

    useForm - Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import { CounterApp } from "./01-useState/CounterApp";
 // import { CounterConCustomHook } from "./01-useState/CounterConCustomHook";
-import { SimpleForm } from "./02-useEffect/SimpleForm";
-
+// import { SimpleForm } from "./02-useEffect/SimpleForm";
+import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 export const App = () => {
   return (
     <>
-      <SimpleForm />
+      <FormWithCustomHook />
     </>
   );
 };
